3e14b4585f3634623ddba85a79b8a4281e9aa4d2
Arthur Silva Mendonça <arthu91@gmail.com>
2023-10-31 16:44:27 -0300

    migrations - commit para instalar MySQL 8.0 e ver se é compatível com Flyway

---
+++ java-Spring-CRUD-api/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,8 @@
+INSERT INTO person (address,first_name,gender,last_name,birth_date) VALUES
+	 ('Sao Paulo/SP','Ana','Female','Hickmann',NULL),
+	 ('Sao Paulo/SP','Tia','Female','Zinha',NULL),
+	 ('Kentucky - US','Mohammed','Male','Ali',NULL),
+	 ('Smiljan - Croatia','Nikola','Male','Tesla',NULL);
+
+	 
+	
