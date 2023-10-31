aa1bad87a8400015393c7e3dad6fc1c3e67d11de
Arthur Silva Mendonça <arthu91@gmail.com>
2023-10-31 18:10:22 -0300

    migrations aplicadas com plugin flyway

--- java-Spring-CRUD-api/src/main/resources/db/migration/V2__Populate_Table_Person.sql
+++ java-Spring-CRUD-api/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -1,8 +1,12 @@
-INSERT INTO person (address,first_name,gender,last_name,birth_date) VALUES
-	 ('Sao Paulo/SP','Ana','Female','Hickmann',NULL),
-	 ('Sao Paulo/SP','Tia','Female','Zinha',NULL),
-	 ('Kentucky - US','Mohammed','Male','Ali',NULL),
-	 ('Smiljan - Croatia','Nikola','Male','Tesla',NULL);
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'São Paulo', 'Ayrton', 'Male', 'Senna'),
+	(2, 'Anchiano - Italy', 'Leonardo', 'Male', 'da Vinci'),
+	(4, 'Porbandar - India', 'Indira', 'Female', 'Gandhi'),
+	(5, 'Porbandar - India', 'Mahatma', 'Male', 'Gandhi'),
+	(7, 'Kentucky - US', 'Muhammad', 'Male', 'Ali'),
+	(9, 'Mvezo – South Africa', 'Nelson', 'Male', 'Mvezo'),
+	(10, 'Smiljan - Croácia', 'Nikola', 'Male', 'Tesla');
+
 
 	 
 	
